5120c49e17c96f312b1511ef660e677513b32d68
august <august@august-sh1tbox>
2025-10-23 12:02:36 -0500

    updated cornell.el, lexical-binding, no more summary, local var

--- cornell.el
+++ cornell.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t; -*-
 (defun conv/cornell-split-file (file-name)
   "returns a list consisting of the name of a file before the extension and the extension. If file-name does not contain a `.`, returns nil"
   (s-index-of "." file-name)
@@ -8,13 +9,9 @@
   "makes windows for cornell style notes"
   (interactive)
   (if (not (f-this-file))
-	(message "this buffer is not a file?")
+	  (message "this buffer is not a file?")
 	(delete-other-windows)
-	(split-window-below 32)
 	(split-window-horizontally 120)
 	(other-window 1)
-	(setq conv/cornell-buffer-name-list (conv/cornell-split-file (buffer-name)))
-	(find-file (concat (car conv/cornell-buffer-name-list) "-cues" (car (cdr conv/cornell-buffer-name-list))))
-	(other-window 1)
-	(find-file (concat (car conv/cornell-buffer-name-list) "-summary" (car (cdr conv/cornell-buffer-name-list))))
-    (other-window 1)))
+	(let '(conv/cornell-buffer-name-list (conv/cornell-split-file (buffer-name)))
+	  (find-file (concat (car conv/cornell-buffer-name-list) "-cues" (car (cdr conv/cornell-buffer-name-list)))))))
